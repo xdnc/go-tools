f6bb0c437d73eb4643d48ae85bfe93e1da077b2f
xiandaonancheng <943719387@qq.com>
2024-04-24 01:26:40 +0800

    init project

--- conv_generator/util/util.go
+++ conv_generator/util/util.go
@@ -1,14 +1,21 @@
 package util
 
-func GetPtr[T interface{}](x T) *T {
-	return &x
+func GetPtr[T interface{}](v T) *T {
+	return &v
 }
 
 func CopyPtr[T interface{}](p *T) *T {
+	if p == nil {
+		return nil
+	}
 	v := *p
 	return &v
 }
 
 func GetVal[T interface{}](p *T) T {
+	if p == nil {
+		var v T
+		return v
+	}
 	return *p
 }
